860599f2fc27353273d0758dad6e2fdb4b353b29
inbalLevi <71599740+inbalLevi@users.noreply.github.com>
2021-08-23 17:56:50 +0300

    Update exercise6-fix.sh

--- vagrant/fixScripts/exercise6-fix.sh
+++ vagrant/fixScripts/exercise6-fix.sh
@@ -1,2 +1,31 @@
 #!/bin/bash
 #add fix to exercise6-fix here
+DST_FOLDER=${@: -1}   # the last parameter
+FILE_LIST=${@:1:$#-1} # all the previous parameters
+
+if [ "$(hostname)" = "server1" ]
+then
+	DST_MACHINE=server2
+	SCP_COMMAND="scp -v -o StrictHostKeyChecking=no"
+elif [ "$(hostname)" == "server2" ]
+then
+	DST_MACHINE=server1
+	SCP_COMMAND="sshpass -p vagrant scp -v -o StrictHostKeyChecking=no"
+fi
+declare -i BYTES
+touch /tmp/output
+
+# copying the files
+
+for file in $FILE_LIST;
+do
+		echo "Transferring file $file"
+		$SCP_COMMAND $file $DST_MACHINE:$DST_FOLDER &>> /tmp/output
+        ((BYTES+=$(cat /tmp/output |  grep -i Transferred | awk '{print $3}' | sed 's/,//'))) &> /dev/null
+done
+
+# Conclusion
+
+echo "Number of bytes that have been transferred:"
+echo $BYTES 
+rm -f /tmp/output
